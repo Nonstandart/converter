80d97a66e5dfea480c6607a2f55be563573a51f4
Илья <ilya.s.2002@mail.ru>
2020-06-15 12:28:25 +0300

    Con

---
+++ Converter_6/app/src/main/java/com/example/converter_6/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.converter_6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun convert(view: View) {
+        val roubles = Roubles.text.toString()
+        if (roubles != "") {
+            val usd = BigDecimal(roubles.toDouble() / 74).setScale(2, RoundingMode.HALF_EVEN)
+            USD.setText(usd.toString())
+        }
+    }
+}
